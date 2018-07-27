9c70bb47419f040458da1790e6ccbaa978ef2df1
BenB <bblkmn5@gmail.com>
2018-07-27 14:28:15 -0700

    add orders#create_controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -3,4 +3,19 @@ class Api::OrdersController < ApplicationController
     @orders = Order.all
     render json: @orders
   end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      render json: @order
+    else
+      render json: { errors: { message: "This Order did not save" }}
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:device, :service, :location, :notes)
+  end
 end
