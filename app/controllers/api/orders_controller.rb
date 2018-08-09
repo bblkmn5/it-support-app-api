4a19fc7aba75038145e51dbc8ef1d5c7bbeaba92
BenB <bblkmn5@gmail.com>
2018-08-09 15:27:16 -0700

    add #update and #destroy methods to orders controller along with private #set_order method, and update Gemfile

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,19 +1,45 @@
 class Api::OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :update, :destroy]  
+  
   def index
     @orders = Order.all
     render json: @orders
   end
 
+  def show
+    render json: @order
+  end
+
   def create
-    @order = Order.new(order_params)
-    if @order.save
+    order = Order.new(order_params)
+    if order.save
+      render json: order
+    else
+      render json: { message: order.errors }
+    end
+  end
+
+  def update
+    if @order.update(order_params)
       render json: @order
     else
-      render json: { errors: { message: "This Order did not save" }}
+      render json: { message: @order.errors }
+    end
+  end
+
+  def destroy
+    if @order.destroy
+      render status: 204 
+    else
+      render json: { message: "This order did not delete. Please try again." }
     end
   end
 
   private
+
+  def set_order
+    @order = Order.find_by(id: params[:id])
+  end
 
   def order_params
     params.require(:order).permit(:device, :service, :location, :notes)
